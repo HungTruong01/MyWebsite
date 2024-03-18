34cfb63020a9e6a4068a0f82da263003730e41bb
HungTruong01 <133994182+HungTruong01@users.noreply.github.com>
2024-03-19 01:00:23 +0700

    Companies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import Header from "./components/Header/Header"; 
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Companies from "./components/Companies/Companies";
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
+      <div>
+        <div className="white-gradient"></div>
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
     </div>
   );
 }
